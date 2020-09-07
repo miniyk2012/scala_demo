3587f7496dd82bcd8d6de8dd7c8b2655a90bd888
杨恺 <yangkai02@qutoutiao.net>
2020-09-07 20:59:37 +0800

    [feature]parquet

--- src/main/java/spark_streaming/demo1.scala
+++ src/main/java/spark_streaming/demo1.scala
@@ -1,5 +1,21 @@
 package spark_streaming
 
-class demo1 {
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 
+/**
+ * 文件流
+ */
+object demo1 {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("TestDStream").setMaster("local[2]")
+    val ssc = new StreamingContext(conf, Seconds(20))
+    val lines =
+      ssc.textFileStream("file:///Users/admin/Documents/javaprojects/scala_demo/src/main/resources")
+    val words = lines.flatMap(_.split(" "))
+    val wordCounts = words.map(x => (x, 1)).reduceByKey(_ + _)
+    wordCounts.print()
+    ssc.start()
+    ssc.awaitTermination()
+  }
 }
--- src/main/java/sparksql/hive_demo.scala
+++ src/main/java/sparksql/hive_demo.scala
@@ -1,4 +1,18 @@
-package rdd
-class hive_demo {
+package sparksql
+
+import org.apache.spark.sql.SparkSession
+
+object hive_demo {
+  def main(args: Array[String]): Unit = {
+    val warehouseLocation = "spark-warehouse"
+    val spark = SparkSession.builder()
+      .appName("Spark Hive Example")
+      .master("local")
+//      .config("spark.sql.warehouse.dir", warehouseLocation)
+      .config("hive.metastore.uris", "thrift://zjka-cpc-backend-bigdata-qa-01:9083")
+      .enableHiveSupport()
+      .getOrCreate()
+    spark.sql("SELECT * FROM sparktest.student")
+  }
 
 }
